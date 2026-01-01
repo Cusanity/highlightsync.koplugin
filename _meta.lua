5f788c57c42ed1387f4fcac7b6c85462227d9c3f
Yanchao Wang <wyc935398521@gmail.com>
2026-01-01 14:35:01 -0600

    Add localization support with highlightsync_gettext module

---
+++ _meta.lua
@@ -0,0 +1,7 @@
+-- Plugin metadata for Highlight Sync
+local _ = require("highlightsync_gettext")
+return {
+    name = "highlightsync",
+    fullname = _("Highlight Sync"),
+    description = _("Synchronize book highlights across devices using cloud storage (WebDAV/Dropbox)."),
+}
